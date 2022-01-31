c2ba6a33e4bc762c76ee6cb180ff445aba060d13
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2022-01-31 22:24:18 +0300

    correct

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import gsdDecription from '../src/games/gcd-description.js';
+
+gsdDecription();
--- bin/brain-gsd.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import gsdDecription from '../src/games/gsd-description.js';
-
-gsdDecription();
