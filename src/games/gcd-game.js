250bde03d0ceeb28fccac54b5e69e897a0212707
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2022-02-07 20:59:40 +0300

    correct

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,30 +1,19 @@
-import _ from 'lodash';
 import { getRandomNumber, commonBrainRules } from '../index.js';
 
 const gameRules = 'Find the greatest common divisor of given numbers.';
 
 const listDivider = (a, b) => {
-  const arrDividerNum1 = [];
-  const arrDividerNum2 = [];
-  for (let i = 0; i <= a; i += 1) {
-    if (a % (a - i) === 0) {
-      arrDividerNum1.push(a - i);
-    }
+  if (!b) {
+    return a;
   }
-
-  for (let j = 0; j <= b; j += 1) {
-    if (b % (b - j) === 0) {
-      arrDividerNum2.push(b - j);
-    }
-  }
-  return (_.intersection(arrDividerNum1, arrDividerNum2));
+  return listDivider(b, a % b);
 };
 
 const infoForGame = () => {
   const number1 = getRandomNumber(1, 100);
   const number2 = getRandomNumber(1, 100);
   const question = (`${number1} ${number2}`);
-  const correctAnswer = String(listDivider(number1, number2)[0]);
+  const correctAnswer = String(listDivider(number1, number2));
   return [question, correctAnswer];
 };
 
